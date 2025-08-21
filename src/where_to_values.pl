4204771ec5406da8e7d0fb0a9ff6ceb00cdfb987
David Ibokette <114680837+david-ibokette@users.noreply.github.com>
2025-08-21 15:50:05 -0400

    Added comments to explain the usage of where_to_values.pl script.

--- src/where_to_values.pl
+++ src/where_to_values.pl
@@ -3,6 +3,15 @@ use strict;
 use feature ":5.10";
 use warnings FATAL => 'all';
 
+# Usage: cat a.txt | where_to_values.pl
+#
+# Where the contents of a.txt is the line below (obtained from DataGrip's "Where clause" extractor). The output of the script will be the
+# SELECT clause beneath. Note: maybe should create a custom extractor in DataGrip - that could be useful.
+
+#
+#     name IN ('Dairy Land', 'State Farm Insurance Company', 'USAA', 'Ahoy!', 'Superior Flood', 'GAINSCO', 'AIG Exchange Private Client', 'Evolve', 'Employers Insurance Group', 'Highland Insurance Solutions', 'RTSpeciality', 'Great American Insurance', 'Victor Insurance', 'Pacific Speciality', 'Liberty Mutual Insurance Co', 'American Family Insurance', 'American Modern Insurance Co', 'Auto Owners Insurance', 'Erie Insurance Group', 'SWBC Insurance Line Setup', 'NEXT', 'Chubb')
+#
+
 # SELECT column1
 #FROM (VALUES 
 #    ('352827533'),
@@ -31,9 +40,6 @@ use warnings FATAL => 'all';
 #    ('355514611'),
 #    ('355948346')
 #) AS t(column1)
-#
-#     name IN ('Dairy Land', 'State Farm Insurance Company', 'USAA', 'Ahoy!', 'Superior Flood', 'GAINSCO', 'AIG Exchange Private Client', 'Evolve', 'Employers Insurance Group', 'Highland Insurance Solutions', 'RTSpeciality', 'Great American Insurance', 'Victor Insurance', 'Pacific Speciality', 'Liberty Mutual Insurance Co', 'American Family Insurance', 'American Modern Insurance Co', 'Auto Owners Insurance', 'Erie Insurance Group', 'SWBC Insurance Line Setup', 'NEXT', 'Chubb')
-#
 while (<STDIN>) {
 	chomp(my $line = $_);
 	$line =~ s/^[^(]*\(//;
