175a89627cecfeab6728baabfcb61e1abd165012
David Ibokette <114680837+david-ibokette@users.noreply.github.com>
2025-08-21 15:46:57 -0400

    Added where_to_values.pl script for processing input into SQL VALUES syntax.

---
+++ src/where_to_values.pl
@@ -0,0 +1,44 @@
+#!/usr/bin/perl -w
+use strict;
+use feature ":5.10";
+use warnings FATAL => 'all';
+
+# SELECT column1
+#FROM (VALUES 
+#    ('352827533'),
+#    ('352718792'),
+#    ('344737704'),
+#    ('344737503'),
+#    ('355516724'),
+#    ('344745311'),
+#    ('349581841'),
+#    ('344745167'),
+#    ('344745360'),
+#    ('349580883'),
+#    ('344745944'),
+#    ('344745050'),
+#    ('349581732'),
+#    ('344744998'),
+#    ('355515053'),
+#    ('344741902'),
+#    ('349583145'),
+#    ('344740083'),
+#    ('355514323'),
+#    ('356172466'),
+#    ('344738972'),
+#    ('349581735'),
+#    ('344738338'),
+#    ('355514611'),
+#    ('355948346')
+#) AS t(column1)
+#
+#     name IN ('Dairy Land', 'State Farm Insurance Company', 'USAA', 'Ahoy!', 'Superior Flood', 'GAINSCO', 'AIG Exchange Private Client', 'Evolve', 'Employers Insurance Group', 'Highland Insurance Solutions', 'RTSpeciality', 'Great American Insurance', 'Victor Insurance', 'Pacific Speciality', 'Liberty Mutual Insurance Co', 'American Family Insurance', 'American Modern Insurance Co', 'Auto Owners Insurance', 'Erie Insurance Group', 'SWBC Insurance Line Setup', 'NEXT', 'Chubb')
+#
+while (<STDIN>) {
+	chomp(my $line = $_);
+	$line =~ s/^[^(]*\(//;
+	$line =~ s/\)\s*$//;
+	$line =~ s/', *'/'),\n('/g;
+	$line = "(" . $line . ")";
+	say "SELECT column1\nFROM(VALUES\n$line\n) AS t(column1)";
+}
